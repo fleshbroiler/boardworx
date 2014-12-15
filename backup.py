a4eed3d8aad38433411c35cb90177055d0e4ce9a
andy <andy@asgard.(none)>
2014-12-14 23:39:35 -0500

    added backup.py and helper object

---
+++ backup.py
@@ -0,0 +1,24 @@
+#backup the /xos dir to /data if sufficient space exists
+
+import os
+import filehelp
+import time
+
+targetPath = '/home/andy/dev/xos'
+destinationPath = '/home/andy/dev/data/'
+
+# get the uncompressed size in bytes of the directory to be backed up... 
+targetBytes = os.path.getsize(targetPath)
+
+#get the size in bytes of the remaining space on the destination device
+remainingBytes = filehelp.get_free_space_bytes(destinationPath) 
+
+print "xos dir size in bytes: " 
+print targetBytes 
+print remainingBytes
+
+if targetBytes < remainingBytes:
+	#since we have enough room to back up the target to the destination, get a filename
+	today = destinationPath + time.strftime("%m")+time.strftime("%d")+time.strftime("%y")+"_"+time.strftime("%H")+time.strftime("%M")+time.strftime("%S")+"bak.tar.gz" 
+	filehelp.zipdir(today, targetPath)
+	
